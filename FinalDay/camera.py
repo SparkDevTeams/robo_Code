0874dbc27873b81c2a46b4d2972ffc8977e5e7ec
korti029 <42555193+korti029@users.noreply.github.com>
2018-11-06 18:00:50 -0500

    useable camera

--- FinalDay/camera.py
+++ FinalDay/camera.py
@@ -6,6 +6,9 @@ camera = PiCamera()
 try:
     while True:
         camera.start_preview()
+        sleep(30)
+        camera.stop_preview()
+        sleep(0.5)
 
 except KeyboardInterrupt:
     camera.stop_preview()
